e56623e085951efa600af7809e50c15ad1ab8dcd
oigen88 <sazukinpavelal@gmail.com>
2023-03-11 18:42:50 +0300

    add clients table

--- src/components/tables/ClientsTable.vue
+++ src/components/tables/ClientsTable.vue
@@ -1,13 +1,37 @@
 <template>
-$END$
+  <my-table
+    :selected="selected"
+    @update:selected="emit('update:selected', $event)"
+    :headers="clientsHeaders"
+    :loading="loading"
+    :items="items"
+    @refresh="emit('refresh')"
+    show-refresh-button
+  />
 </template>
 
-<script>
-export default {
-name: "ClientsTable"
-}
+<script setup lang="ts">
+import myTable from "@/components/table.vue";
+import { defineProps, toRefs, defineEmits } from "vue";
+
+const props = defineProps({
+  items: { type: Array, required: true },
+  selected: { type: Array, required: true },
+  loading: { type: Boolean, default: false },
+});
+
+const { items, loading } = toRefs(props);
+
+const emit = defineEmits(["update:selected", "refresh"]);
+
+const clientsHeaders: any = [
+  { title: "Логин", value: "login" },
+  { title: "Фио", value: "fio" },
+  { title: "Возвраст", value: "age" },
+  { title: "Вес", value: "weight" },
+  { title: "Рост", value: "height" },
+  { title: "Питание", value: "diet.name" },
+];
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
