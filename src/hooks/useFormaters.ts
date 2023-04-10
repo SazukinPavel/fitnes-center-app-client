f015c63bf2f4b2499b3e36502a17a6307ff3531d
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-10 12:29:30 +0300

    add format date hook

---
+++ src/hooks/useFormaters.ts
@@ -0,0 +1,12 @@
+import moment from "moment";
+
+export default function useValidators() {
+  const formatDate = (date: any) => {
+    return moment(date).format("YYYY-MM-DD");
+  };
+  const formatDateTime = (datetime: any) => {
+    return moment(datetime).utc().format("YYYY-MM-DD HH:mm");
+  };
+
+  return { formatDate, formatDateTime };
+}
