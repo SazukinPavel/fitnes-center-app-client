844d3729afebebf8da585cc43b8b7df44201ce50
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-10 15:39:34 +0300

    add clients exercises cards

---
+++ src/components/clientExerciseCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="ma-4">
+    <v-card-title>{{ props.exercise.exerciseInfo.name }}</v-card-title>
+    <v-card-title>Дата: {{ formatDateTime(props.exercise.date) }}</v-card-title>
+    <v-card-title>Тренер: {{ props.manager.auth.fio }}</v-card-title>
+    <v-expansion-panels>
+      <v-expansion-panel title="Описание">
+        <v-expansion-panel-text>
+          {{ props.exercise.exerciseInfo.description }}
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { defineProps, PropType } from "vue";
+import { Exercise } from "@/types/Exercise";
+import useFormmaters from "@/hooks/useFormaters";
+import Manager from "@/types/Manager";
+
+const props = defineProps({
+  exercise: { type: Object as PropType<Exercise>, required: true },
+  manager: { type: Object as PropType<Manager>, required: true },
+});
+
+const { formatDateTime } = useFormmaters();
+</script>
+
+<style scoped></style>
--- src/views/ClientsExercises.vue
+++ src/views/ClientsExercises.vue
@@ -1,42 +1,22 @@
 <template>
-  <v-row class="d-flex justify-center flex-column mt-5">
-    <v-card-title class="text-center">Занятия:</v-card-title>
-    <DatePicker
-      class="my-5 mx-auto"
-      @dayclick="selectDay"
-      :attributes="exercises"
-      color="blue"
-    ></DatePicker>
-  </v-row>
-  <v-expansion-panels variant="popout" class="my-4">
-    <v-expansion-panel v-for="ex in selectedExercises" :key="ex.id">
-      <v-expansion-panel-title>
-        <v-card-title
-          >{{ moment(ex.date).format("HH-MM") }} ||
-          {{ ex.exerciseInfo?.name }}</v-card-title
-        >
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <v-card-title
-          >Время : {{ moment(ex.date).format("HH-MM") }}</v-card-title
-        >
-        <v-card-title>Тренировка : {{ ex.exerciseInfo?.name }}</v-card-title>
-
-        <v-card-text class="text-h6"
-          >Описание : {{ ex.exerciseInfo?.description }}</v-card-text
-        >
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-  </v-expansion-panels>
+  <v-card class="ma-0 pa-0" :loading="isExercisesLoading">
+    <v-card-actions> </v-card-actions>
+    <div>
+      <client-exercise-card
+        v-for="exercise in exercises"
+        :key="exercise.id"
+        :manager="exercise.manager"
+        :exercise="exercise"
+      />
+    </div>
+  </v-card>
 </template>
 
 <script setup lang="ts">
-import { DatePicker } from "v-calendar";
-import { computed, ComputedRef, onMounted, ref } from "vue";
 import { Exercise } from "@/types/Exercise";
-const selectedDate = ref<null | Date>(null);
-import moment from "moment";
 import { useStore } from "vuex";
+import { computed, onMounted } from "vue";
+import ClientExerciseCard from "@/components/clientExerciseCard.vue";
 
 const store = useStore();
 
@@ -44,34 +24,12 @@ onMounted(() => {
   store.dispatch("exercises/fetch");
 });
 
-const selectDay = (val: any) => {
-  const selected: any = exercises.value.find((e) => {
-    return moment(e.dates).format("YYYY-MM-DD") == val.id;
-  });
-
-  if (selected) {
-    selectedDate.value = selected.dates;
-  }
-};
-
-const exercises: ComputedRef<any> = computed(() => {
-  return store.getters["exercises/exercises"].map((e: Exercise) => ({
-    highlight: {
-      color: "blue",
-      fillMode: "outline",
-    },
-    dates: e.date,
-  }));
-});
-
-const selectedExercises = computed(() => {
-  return store.getters["exercises/exercises"].filter((e: any) => {
-    return (
-      moment(e.date).format("DD-MM-YY") ===
-      moment(selectedDate.value).format("DD-MM-YY")
-    );
-  });
-});
+const exercises = computed<Exercise[]>(
+  () => store.getters["exercises/exercises"]
+);
+const isExercisesLoading = computed<boolean>(
+  () => store.getters["exercises/isLoading"]
+);
 </script>
 
 <style scoped></style>
