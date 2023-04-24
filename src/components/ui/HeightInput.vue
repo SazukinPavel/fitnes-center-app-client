a33086ffab8a4df7358f94fedfb4ac401a6cf532
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-24 21:59:04 +0300

    add height input

---
+++ src/components/ui/HeightInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <number-input
+    class="my-2"
+    label="Рост"
+    :rules="[requiredRule, heightRule]"
+    :value="props.value"
+    suffix="сантиметров"
+    @update:value="emit('update:value', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+import NumberInput from "@/components/ui/NumberInput.vue";
+import { defineEmits, defineProps } from "vue";
+import useValidators from "@/hooks/useValidators";
+
+const { heightRule, requiredRule } = useValidators();
+
+const props = defineProps({
+  value: { type: Date, default: null },
+});
+const emit = defineEmits(["update:value"]);
+</script>
+
+<style scoped></style>
