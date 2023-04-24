d78c8a3c1e56389bcdf6f7d2d55227ece1d371ee
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-24 19:20:13 +0300

    fix date picker

--- src/components/ui/datePicker.vue
+++ src/components/ui/datePicker.vue
@@ -1,89 +1,46 @@
 <template>
-  <vue-date-picker
-    :enable-time-picker="enableTimePicker"
-    input-class-name="date-picker"
-    teleport-center
-    :placeholder="placeholder"
-    :min-date="minDate"
-    :max-date="maxDate"
-    offset="20"
-    position="right"
-    :model-value="value"
-    @input="emit('update:value', $event)"
-    locale="ru-RU"
-    ref="datepicker"
-    :start-date="maxDate"
-  >
-    <template #input-icon>
-      <v-icon>mdi-calendare</v-icon>
-    </template>
-  </vue-date-picker>
+  <v-text-field
+    :type="inputType"
+    :model-value="props.value"
+    @input="emit('update:value', $event.target.value)"
+    :min="minDate"
+    :max="maxDate"
+    :rules="rules"
+  ></v-text-field>
 </template>
 
 <script setup lang="ts">
-import { defineProps, computed, onMounted, defineEmits, ref } from "vue";
+import { defineProps, defineEmits, computed } from "vue";
 import moment from "moment";
-import { DatePickerInstance } from "@vuepic/vue-datepicker";
 
 const props = defineProps({
-  type: String,
+  type: { String, required: true },
   value: { type: Date, default: null },
   placeholder: { type: String, default: "" },
+  rules: { type: Array, default: () => [] },
 });
 const emit = defineEmits(["update:value"]);
 
-const datepicker = ref<DatePickerInstance>(null);
+const momentDatePattern = "YYYY-MM-DD";
+const momentDateTimePattern = "YYYY-MM-DDTHH:mm";
 
-const maxDate = computed<Date | string>(() => {
+const inputType = computed(() => {
+  return props.type == "BirthDate" ? "date" : "datetime-local";
+});
+
+const maxDate = computed<string>(() => {
   if (props.type == "Meeting") {
-    return "";
+    return moment().add(1, "year").format(momentDateTimePattern);
   }
 
-  return moment().subtract(18, "years").toDate();
+  return moment().subtract(18, "years").format(momentDatePattern);
 });
-
-const minDate = computed<Date | string>(() => {
+const minDate = computed<string>(() => {
   if (props.type == "BirthDate") {
-    return "";
+    return moment().subtract(100, "years").format(momentDatePattern);
   }
-  return moment().toDate();
-});
-
-const enableTimePicker = computed(() => {
-  return props.type !== "BirthDate";
-});
-
-onMounted(() => {
-  emit("update:value", null);
-
-  if (datepicker.value) {
-    datepicker.value.setMonthYear({
-      month: moment(maxDate.value).month(),
-      year: moment(maxDate.value).year(),
-    });
-  }
+  return moment().format(momentDateTimePattern);
 });
 </script>
 
-<style lang="scss">
-.date-picker {
-  text-align: center;
-  margin-bottom: 25px;
-  letter-spacing: 0.009375em;
-  min-height: var(--v-input-control-height, 56px);
-  width: 100%;
-  border: 1px solid rgb(171, 171, 171) !important;
-  &:focus,
-  :hover,
-  :active {
-    border-color: #083ef3;
-  }
-}
-:root {
-  /*General*/
-  --dp-font-family: -apple-system, blinkmacsystemfont, "Segoe UI", roboto,
-    oxygen, ubuntu, cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  --dp-border-radius: 4px; /*Configurable border-radius*/
-  --dp-cell-border-radius: 4px; /*Specific border radius for the calendar cell*/
-}
-</style>
+<style lang="scss"></style>
