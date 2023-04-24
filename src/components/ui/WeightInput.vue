6adba01c0837267afd697da1f963bb47d01cd2cf
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-24 21:59:16 +0300

    add weight input

---
+++ src/components/ui/WeightInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <number-input
+    class="my-2"
+    :rules="[requiredRule, weightRule]"
+    label="Вес"
+    :value="props.value"
+    @update:value="emit('update:value', $event)"
+    suffix="килограмм"
+  />
+</template>
+
+<script setup lang="ts">
+import NumberInput from "@/components/ui/NumberInput.vue";
+import { defineEmits, defineProps } from "vue";
+import useValidators from "@/hooks/useValidators";
+
+const { weightRule, requiredRule } = useValidators();
+
+const props = defineProps({
+  value: { type: Date, default: null },
+});
+const emit = defineEmits(["update:value"]);
+</script>
+
+<style scoped></style>
