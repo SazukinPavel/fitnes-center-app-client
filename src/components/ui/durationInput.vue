3ed9c6abb49bc12a6f6c452b258f1091b51e8f71
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-27 11:52:58 +0300

    add duration input

---
+++ src/components/ui/durationInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <number-input
+    :rules="[requiredRule, durationRule]"
+    :value="props.value"
+    suffix="минут"
+    label="Длительность тренировки"
+    @update:value="emit('update:value', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+import NumberInput from "@/components/ui/NumberInput.vue";
+import useValidators from "@/hooks/useValidators";
+import { defineEmits, defineProps } from "vue";
+
+const { durationRule, requiredRule } = useValidators();
+
+const props = defineProps({
+  value: { type: Date, default: null },
+});
+const emit = defineEmits(["update:value"]);
+</script>
+
+<style scoped></style>
