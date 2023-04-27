6ac59269460f4e2747027a19918f69ce01971989
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-27 11:33:51 +0300

    update diet page

--- src/views/DietPage.vue
+++ src/views/DietPage.vue
@@ -1,33 +1,19 @@
 <template>
-  <v-layout full-height>
-    <v-container>
-      <v-card class="ma-5 pa-5">
-        <v-card-title class="text-center">Диета:</v-card-title>
-        <v-card-title>{{ client.diet?.name }}</v-card-title>
-        <v-card-text class="text-h6">{{
-          client.diet?.description
-        }}</v-card-text>
-      </v-card>
-    </v-container>
-  </v-layout>
+  <v-card variant="text">
+    <v-card-title class="text-center">Диета:</v-card-title>
+    <v-card-title class="text-wrap">{{ client.diet?.name }}</v-card-title>
+    <v-card-text>{{ client.diet?.description }}</v-card-text>
+  </v-card>
 </template>
 
 <script setup lang="ts">
 import { useStore } from "vuex";
-import { computed, onMounted, onUnmounted } from "vue";
+import { computed } from "vue";
 import Client from "@/types/entitys/Client";
 
 const store = useStore();
 
 const client = computed<Client>(() => store.getters["auth/user"]);
-
-onMounted(() => {
-  store.commit("app/setIsBackButtonShow", true);
-});
-
-onUnmounted(() => {
-  store.commit("app/setIsBackButtonShow", false);
-});
 </script>
 
 <style scoped></style>
