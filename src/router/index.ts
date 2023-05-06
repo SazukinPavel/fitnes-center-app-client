844ba4937c041c89ec8f9b31cdb51dca8ea84968
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-05-06 20:35:06 +0300

    fix router history (no #)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/views/Login.vue";
 import DietPage from "@/views/DietPage.vue";
 import Diets from "@/views/Diets.vue";
@@ -131,7 +131,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes, // short for `routes: routes`
 });
 
