9bdc13cb330ceab3534005d567d48e1af51250b2
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-05-09 14:03:31 +0300

    refactor & add avatar

--- src/views/ManagerPage.vue
+++ src/views/ManagerPage.vue
@@ -1,12 +1,15 @@
 <template>
   <v-card variant="text">
     <v-card-title class="text-center">Тренер:</v-card-title>
-    <v-card-title class="text-wrap">{{ manager.owner.auth.fio }}</v-card-title>
+    <div class="d-flex justify-center align-center">
+      <v-avatar v-if="avatarImage" :image="avatarImage" size="200"></v-avatar>
+    </div>
+    <v-card-title class="text-wrap">{{ manager?.auth.fio }}</v-card-title>
     <v-card-title class="text-wrap">Возраст: {{ managerAge }}</v-card-title>
     <v-card-title class="text-wrap"
-      >Телефонный номер: {{ manager.owner.auth.telephone }}</v-card-title
+      >Телефонный номер: {{ manager?.auth.telephone }}</v-card-title
     >
-    <v-card-text>{{ manager.owner.description }}</v-card-text>
+    <v-card-text>{{ manager?.description }}</v-card-text>
   </v-card>
 </template>
 
@@ -15,14 +18,19 @@ import { useStore } from "vuex";
 import { computed } from "vue";
 import Client from "@/types/entitys/Client";
 import moment from "moment";
+import Manager from "@/types/entitys/Manager";
+import useUserAvatar from "@/hooks/userAvatar";
 
 const store = useStore();
 
-const manager = computed<Client>(() => store.getters["auth/user"]);
+const user = computed<Client>(() => store.getters["auth/user"]);
+const manager = computed<Manager | undefined>(() => user.value?.owner);
 
 const managerAge = computed(() => {
-  return moment().diff(manager.value?.owner?.auth?.birthDate, "years");
+  return moment().diff(manager.value?.auth?.birthDate, "years");
 });
+
+const avatarImage = useUserAvatar(manager.value);
 </script>
 
 <style scoped></style>
